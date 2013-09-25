c8f5ba7a3ff75f6c690a04862b25952220117fdd
Zachary Scott <zachary@zacharyscott.net>
2013-09-25 12:38:26 -0600

    move stuff to Game module to make class simpler

--- mojibake.shoes.rb
+++ mojibake.shoes.rb
@@ -1,47 +1,69 @@
-class Mojibake
-  attr_accessor :app, :pick
-
-  def initialize app
-    @app = app
-    hiragana = Dir["images/hiragana/*.png"]
-    @pick = File.basename(hiragana.sample).delete('.png')
-    render_welcome
-  end
-
-  def render_welcome
-    clear
-    @app.stack do
-      @app.image "logo.jpg", align: "center"
-      @app.button "continue", width: 300 do
-        clear
-        render_quiz
-      end
-      @app.image "bg.jpg", align: "center", margin_top: 55
-    end
-  end
-
-  def render_quiz
-    @app.image "images/hiragana/#{@pick}.png", width: 300, height: 300
-    guess = @app.edit_line width: 290
-    @app.button "Guess", width: 300 do
-      if guess.text == @pick
-        @app.alert "Congratulations!"
-      else
-        @app.alert "Wrong! The correct answer was: #{@pick}."
-      end
-      @app.new_game
-    end
-  end
+module Game
+  OPTIONS = {width: 300, height: 380}
 
   def clear
     @app.clear
     @app.background "#fff"
   end
+
+  def restart
+    @app.new_game
+  end
+
+  def welcome
+    clear
+    @app.stack do
+      @app.image "logo.jpg", align: "center"
+      @app.button "continue", width: 300 do
+        clear
+        yield
+      end
+      @app.image "bg.jpg", align: "center", margin_top: 55
+    end
+  end
+
+  def set_random_pick
+    hiragana = Dir["images/hiragana/*.png"]
+    pick = File.basename(hiragana.sample).delete('.png')
+    pick
+  end
+
+  def render_pick(pick)
+    @app.image "images/hiragana/#{pick}.png", width: 300, height: 300
+  end
 end
 
-Shoes.app width: 300, height: 380 do
+class Mojibake
+  include Game
+
+  attr_accessor :app
+
+  def initialize app
+    @app = app
+    welcome do
+      pick = set_random_pick
+      render_pick pick
+
+      guess = @app.edit_line width: 290
+      @app.button "Guess", width: 300 do
+        check_guess(guess.text, pick)
+        restart
+      end
+    end
+  end
+
+  def check_guess(guess, pick)
+    if guess == pick
+      @app.alert "Congratulations!"
+    else
+      @app.alert "Wrong! The correct answer was: #{pick}."
+    end
+  end
+end
+
+Shoes.app Game::OPTIONS do
   def new_game
-    @mojibake = Mojibake.new self
+    Mojibake.new self
   end
   new_game
 end

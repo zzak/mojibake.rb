84fcef4f1d7d93353a68ec44663a0d9a28a088e7
Zachary Scott <zachary@zacharyscott.net>
2014-01-04 20:51:17 -0800

    * Move Game module to ./helpers folder * Move welcome logo and background image to ./images * Update shoes app to reflect these changes

--- mojibake.shoes.rb
+++ mojibake.shoes.rb
@@ -1,37 +1,4 @@
-module Game
-  OPTIONS = {width: 300, height: 380}
-
-  def clear
-    @app.clear
-    @app.background "#fff"
-  end
-
-  def restart
-    @app.new_game
-  end
-
-  def welcome
-    clear
-    @app.stack do
-      @app.image "logo.jpg", align: "center"
-      @app.button "continue", width: 300 do
-        clear
-        yield
-      end
-      @app.image "bg.jpg", align: "center", margin_top: 55
-    end
-  end
-
-  def set_random_pick
-    hiragana = Dir["images/hiragana/*.png"]
-    pick = File.basename(hiragana.sample).delete('.png')
-    pick
-  end
-
-  def render_pick(pick)
-    @app.image "images/hiragana/#{pick}.png", width: 300, height: 300
-  end
-end
+require 'helpers/game'
 
 class Mojibake
   include Game
